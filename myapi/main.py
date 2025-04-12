39cad49c811edf93ba1fc60680d1731d4e581090
Priyanchew <priyanshu6beta@gmail.com>
2025-04-12 20:03:01 +0530

    removed myapi

--- myapi/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routes import users, teams, assignments, submissions
-
-app = FastAPI()
-
-app.include_router(users.router)
-app.include_router(teams.router)
-app.include_router(assignments.router)
-app.include_router(submissions.router)
