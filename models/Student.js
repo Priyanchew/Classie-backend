909b00af0159d2f3e5f0d673738ce6f37f9ca8d9
jaskaran-01 <jaskarantaneja01@gmail.com>
2025-04-12 15:06:56 +0530

    deleted changes

--- models/Student.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const studentSchema = new mongoose.Schema({
-  studentId: { type: String, unique: true },
-  name: String,
-  email: String
-});
-
-module.exports = mongoose.model('Student', studentSchema);
